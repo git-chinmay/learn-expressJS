bb31c37dbb1dbf04a7d62c383b9092c036fdde38
CICADA3310 <36794918+git-chinmay@users.noreply.github.com>
2023-04-13 10:12:04 +0530

    database setup

---
+++ module-2/index.js
@@ -0,0 +1,10 @@
+/*
+PREREQUSITES:
+- Install mongodb and start it
+- Run compass 
+- Create a test db
+*/
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/my-db-cts');
+
