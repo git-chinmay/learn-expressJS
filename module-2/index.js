21ed89058bc181110278099497cee88f167c4dee
CICADA3310 <36794918+git-chinmay@users.noreply.github.com>
2023-04-13 11:51:32 +0530

    Adding data into database from form field.

--- module-2/index.js
+++ module-2/index.js
@@ -6,5 +6,66 @@ PREREQUSITES:
 */
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/my-db-cts');
+const express = require('express');
+const hbs = require('handlebars')
+const bodyParser = require('body-parser');
+
+//Make sure mongodb engine is up and running in your terminal
+mongoose.connect('mongodb://127.0.0.1:27017/my-db-cts');
+
+
+//Setting up the app
+const app = express();
+app.set('view engine', 'hbs');
+app.set('views', './views');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+const port = 3005;
+
+
+//Create schema
+const personSchema = mongoose.Schema({
+    name:String,
+    age:Number,
+    nationality:String
+})
+
+//Create Model
+const Person = mongoose.model("Person", personSchema);
+
+app.get('/person', (req, res)=>{
+    res.render('person');
+ });
+
+
+app.post('/person', async (req, res)=>{
+    var personInfo = req.body; //Get the parsed information
+    console.log(req);
+    if(!personInfo.name || !personInfo.age || !personInfo.nationality){
+       res.render('show_message', {
+          message: "Sorry, you provided worng info", type: "error"});
+    } 
+    else {
+       var newPerson = new Person({
+          name: personInfo.name,
+          age: personInfo.age,
+          nationality: personInfo.nationality
+       });
+         
+       // Saving the document in db
+        try{
+            await newPerson.save();
+            res.render('show_message', {message: "New person added", type: "success", person: personInfo});
+        }
+        catch{
+            res.render('show_message', {message: "Database error", type: "error"});
+        }
+    }
+    
+});
+
+app.listen(port, ()=>{
+    console.log(`Server listenong on port ${port}`);
+})
 
